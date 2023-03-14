11f69e1de9a27975216cd190724d3a2232bd334c
Shaxzod1311 <sh.nematullayev@baik.uz>
2023-03-14 18:42:44 +0500

    setting default value into seed data for testing

--- E-Wallet.Data/Data/WalletDbSeed.cs
+++ E-Wallet.Data/Data/WalletDbSeed.cs
@@ -23,13 +23,13 @@ namespace E_Wallet.Data.Data
             }
 
 
-            var user1 = new User { Username = "john", Password = "pass123", Name = "John Smith", Phone = "+1234567890", IsIdentified = true };
+            var user1 = new User { Id = Guid.Parse("2ba5597b-5a8b-45c3-a838-c886a4873acf"), Username = "john", Password = "pass123", Name = "John Smith", Phone = "+1234567890", IsIdentified = true, SecretKey = "1KpT1XxtIm+PYLn98xbFtAmrw/ihD7QrBImsA/WFdjU=" };
             var user2 = new User { Username = "jane", Password = "pass456", Name = "Jane Doe", Phone = "+0987654321", IsIdentified = false };
 
             db.Users.AddRange(user1, user2);
             db.SaveChanges();
 
-            var wallet1 = new Wallet { UserId = user1.Id, Balance = 10000 };
+            var wallet1 = new Wallet { Id = Guid.Parse("9d887904-794d-4535-924c-fe44696bcaec"), UserId = user1.Id, Balance = 10000 };
             var wallet2 = new Wallet { UserId = user1.Id, Balance = 5000 };
             var wallet3 = new Wallet { UserId = user2.Id, Balance = 500 };
 
